63f01eef94165ad18efac8284fe3ebf53469c273
Ahmed-HA-RE <ah607k@gmail.com>
2025-10-14 16:42:51 +0400

    feat: first init for frontend project

---
+++ frontend/app/routes/home.tsx
@@ -0,0 +1,14 @@
+import type { Route } from './+types/home';
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: 'New React Router App' },
+    { name: 'description', content: 'Welcome to React Router!' },
+  ];
+}
+
+const HomePage = () => {
+  return <div></div>;
+};
+
+export default HomePage;
